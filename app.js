7e4f7aaf99b3c46a5df42cc48a2e9d58819c1fc3
Nischal Rai <nischalrai04@gmail.com>
2020-01-20 15:20:33 +0100

    resetCalculator function created to reset values

--- app.js
+++ app.js
@@ -4,6 +4,69 @@ const calculator = {
     operator: null,
     waitingForSecondOperand: false
 };
+function inputDigit(digit){
+    const { displayValue, waitingForSecondOperand} = calculator;
+
+    if(waitingForSecondOperand === true){
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false;
+    } else{
+        // Overwrite `displayValue` if the current value is '0' otherwise append to it
+        calculator.displayValue = displayValue === '0'? digit : displayValue + digit;
+    }
+    console.log(calculator);
+}
+function inputDecimal(dot){
+    if(!calculator.displayValue.includes(dot)){
+        calculator.displayValue += dot;
+    }
+}
+function handleOperator(nextOperator) {
+    const { firstOperand, displayValue, operator } = calculator;
+    const inputValue = parseFloat(displayValue);
+
+    if (operator && calculator.waitingForSecondOperand)  {
+        calculator.operator = nextOperator;
+        console.log(calculator);
+        return;
+    }
+
+    if (firstOperand === null) {
+        calculator.firstOperand = inputValue;
+    }
+    else if(operator){
+        const result = performCalculation[operator](firstOperand, inputValue);
+
+        calculator.displayValue = String(result);
+        calculator.firstOperand = result;
+    }
+
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+    console.log(calculator);
+}
+const performCalculation = {
+    '/': function (firstOperand,secondOperand) {
+        return firstOperand/secondOperand;
+    },
+    '*': function (firstOperand,secondOperand) {
+        return firstOperand * secondOperand;
+    },
+    '+': function (firstOperand,secondOperand) {
+        return firstOperand + secondOperand;
+    },
+    '-': function (firstOperand,secondOperand) {
+        return firstOperand - secondOperand;
+    }
+};
+
+function resetCalculator(){
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+}
+
 function updateDisplay(){
     const display = document.querySelector('.calculator-screen');
     display.value = calculator.displayValue;
@@ -17,18 +80,23 @@ keys.addEventListener('click',function(event){
        return;//exit the function
    }
    if(target.classList.contains('operator')){
-       console.log('operator', target.value);
+       handleOperator(target.value);
+       updateDisplay();
        return;
 
    }
    if(target.classList.contains('decimal')){
-       console.log('decimal', target.value);
+       inputDecimal(target.value);
+       updateDisplay();
        return;
    }
     if (target.classList.contains('all-clear')) {
-        console.log('all-clear', target.value);
+        resetCalculator();
+        updateDisplay();
         return;
 
     }
-    console.log('digit',target.value);
+    inputDigit(target.value);
+    updateDisplay();
 });
+
